e4f82c5d07e8773612a53315bcd49442b77cbd8c
Emmanuel Moshood <emmanuelmoshood571@gmail.com>
2023-05-09 22:18:58 -0400

    add description input

--- script.js
+++ script.js
@@ -84,3 +84,75 @@ const removeRow = (cellValue) => {
     selectedRow = cellValue.parentElement.parentElement;
     document.getElementById("result-table").deleteRow(selectedRow.rowIndex);
 }
+
+
+
+/////////////
+const cardForm = document.getElementById('card-form');
+const cardContainer = document.getElementById('card-container');
+let isEditing = false;
+let editingCard = null;
+
+cardForm.addEventListener('submit', function(event) {
+  event.preventDefault(); // prevent form submission
+
+  // get values from form inputs
+  const titleInput = document.getElementById('title-input');
+  const imageInput = document.getElementById('image-input');
+  const descriptionInput = document.getElementById('description-input');
+  const title = titleInput.value;
+  const image = imageInput.value;
+  const description = descriptionInput.value;
+
+  // create card element with title, image, and description
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `
+    <img src="${image}" alt="${title}">
+    <h2>${title}</h2>
+    <p>${description}</p>
+    <button class="edit-button">Edit</button>
+  `;
+
+  // add event listener to edit button
+  const editButton = card.querySelector('.edit-button');
+  editButton.addEventListener('click', function() {
+    isEditing = true;
+    editingCard = card;
+
+    // show form and pre-populate fields with card values
+    cardForm.classList.add('show-form');
+    titleInput.value = card.querySelector('h2').textContent;
+    imageInput.value = card.querySelector('img').getAttribute('src');
+    descriptionInput.value = card.querySelector('p').textContent;
+  });
+
+  if (isEditing && editingCard === card) {
+    // update existing card with new values
+    card.querySelector('img').setAttribute('src', image);
+    card.querySelector('img').setAttribute('alt', title);
+    card.querySelector('h2').textContent = title;
+    card.querySelector('p').textContent = description;
+    isEditing = false;
+    editingCard = null;
+  } else {
+    // append new card to card container
+    cardContainer.appendChild(card);
+  }
+
+  // reset form inputs
+  titleInput.value = '';
+  imageInput.value = '';
+  descriptionInput.value = '';
+});
+
+// add event listener to form cancel button
+const cancelButton = cardForm.querySelector('.cancel-button');
+cancelButton.addEventListener('click', function() {
+  isEditing = false;
+  editingCard = null;
+  cardForm.classList.remove('show-form');
+  titleInput.value = '';
+  imageInput.value = '';
+  descriptionInput.value = '';
+});
